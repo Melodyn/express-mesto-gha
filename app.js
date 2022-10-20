0402ac414c407316347d5258e1c0970a305b11ef
melodyn <samelodyn@gmail.com>
2022-10-20 07:35:57 +0300

    14th;update dep; add log

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ export const run = async (envName) => {
     console.error(err);
     process.exit(1);
   });
-  console.log(process.env);
+  console.log({ PORT: process.env.PORT });
 
   const config = dotenv.config({ path: path.resolve('.env.common') }).parsed;
   if (!config) {
